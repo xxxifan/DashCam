6b211f63d05d803d6ce201d9ef8d824d33a5772d
xxxifan <445947962@qq.com>
2026-08-12 03:17:49 +0800

    chore(release): prepare v0.3.208

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,8 +38,8 @@ android {
         applicationId = "com.xxxifan.dashcam"
         minSdk = 36
         targetSdk = 36
-        versionCode = 2217
-        versionName = "0.2.217"
+        versionCode = 3208
+        versionName = "0.3.208"
         buildConfigField("String", "GITHUB_REPOSITORY", "\"xxxifan/DashCam\"")
 
         ndk {
